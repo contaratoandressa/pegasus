1eb6fd351d7b882f1a965aa072d5abb37a811281
Andressa Contarato <andressa.contarato@fgv.br>
2019-07-31 11:14:30 -0300

    ajustes na funcao

--- auxFunctions/R/joins.R
+++ auxFunctions/R/joins.R
@@ -6,13 +6,13 @@ install_load("dplyr","readxl")
 
 merge_podram <- function(dir = "dir", name1 = "db1", name2 = "db2", var = "id", year = "year_column"){
   
-  db1 <- read_excel(paste0(dir, name2)) ; db2 <- read_excel(paste0(dir, name1))
+  db1 <- read_excel(paste0(dir, name1)) ; db2 <- read.csv(paste0(dir, name2))
   dados <- data.frame()
-  min_year <- min(bd1[,year]) ; max_year <- max(bd1[,year])
+  min_year <- min(db2[,year]) ; max_year <- max(db2[,year])
   
   for(i in min_year:max_year){ 
-    aux <- filter(db2, year_column == i)
-    aux <- full_join(db1, aux, by = var)
+    aux <- db1[which(db1[year] == i),]
+    aux <- full_join(db2, aux, by = var)
     dados <- rbind(dados, aux)
   }
   
